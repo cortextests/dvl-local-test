01a53bddf19bc8f8ec81c17e8e1337164ae8ea96
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-03-22 14:10:57 +0100

    feat: add config.exs

---
+++ config.exs
@@ -0,0 +1,14 @@
+import Config
+
+config :vault_ecto,
+  ecto_repos: [VaultEcto.Repo]
+
+config :vault_ecto, VaultEcto.Repo,
+  pool_size: 10,
+  configure: {VaultEcto.Repo, :configure, []},
+  disconnect_on_error_codes: [
+    :insufficient_privilege,
+    :invalid_authorization_specification,
+    :invalid_password,
+    :read_only_sql_transaction
+  ]
